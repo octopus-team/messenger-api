59fb0276090ea1eebac09e1da69df28ca6a14bd2
Quentin Pre <pre.quentin@gmail.com>
2016-04-30 09:05:06 +0200

    [UPD] reading from env

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -4,12 +4,12 @@ use Mix.Config
 # you likely want to automate and keep it away from
 # your version control system.
 config :messenger, Messenger.Endpoint,
-  secret_key_base: "QoFx+yn6EYs22xOnCXkN/98N7wuvV7JpUzeLGYhwGiAkhs2I6cz3bPbVelsYQZRR"
+  secret_key_base: System.get_env("OCTOPUS_MESSENGER_API_SECRET_KEY_BASE")
 
 # Configure your database
 config :messenger, Messenger.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: System.get_env("DATABASE_USERNAME"),
+  password: System.get_env("DATABASE_PASSWORD"),
   database: "messenger_prod",
   pool_size: 20
