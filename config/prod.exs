489600f4553a9dd8e1b2711d7614b20d783ffa21
Quentin Pre <pre.quentin@gmail.com>
2016-04-30 08:59:22 +0200

    [FIX] typo

--- config/prod.exs
+++ config/prod.exs
@@ -17,7 +17,7 @@ config :messenger, Messenger.Endpoint,
   url: [host: "octopus_messenger_api.herokuapp.com", port: 443],
   secret_key_base: System.get_env("OCTOPUS_MESSENGER_API_SECRET_KEY_BASE")
 
-config :messenger, Messenger.Repo
+config :messenger, Messenger.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: 20
